6a4947bcaee8cbe91848eb57d88fa96fc551ef72
aimon flaherty <aimonflaherty@gmail.com>
2023-06-29 15:20:39 -0400

    adding cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
 
 const movieRouter = require("./movies/movies.router");
 const reviewRouter = require("./reviews/reviews.router");
 const theaterRouter = require("./theaters/theaters.router");
 
 app.use(express.json());
+app.use(cors());
 app.use("/movies", movieRouter);
 app.use("/theaters", theaterRouter);
 app.use("/reviews", reviewRouter);
